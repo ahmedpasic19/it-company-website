31a5dc0bc8b020c2762dd9c94a3354258956a9b3
ahmedpasic19 <ahmedpasic15@gmail.com>
2024-08-26 18:55:11 +0200

    Improved hero section
    Changes:
    - Improved gradient

--- src/layout/navbar.tsx
+++ src/layout/navbar.tsx
@@ -1,5 +1,7 @@
+import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
+import Logo from '@/assets/Screenshot 2024-08-26 at 18.52.35.png'
 
 const Navbar = () => {
   const links = [
