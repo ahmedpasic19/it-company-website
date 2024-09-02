23434b23c57ca0e4a559a40535814525207c08fd
ahmedpasic19 <ahmedpasic15@gmail.com>
2024-09-02 17:16:47 +0200

    Global brands

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,21 @@
 import About from '@/layout/about'
 import Clients from '@/layout/clients'
 import Features from '@/layout/features'
+import GlobalClients from '@/layout/global-clients'
 import HeroSection from '@/layout/hero-section'
 import Portofolio from '@/layout/portofolio'
 import Team from '@/layout/team'
 
 export default function Home() {
   return (
-    <main className=''>
+    <main>
       <HeroSection />
       <Features />
       <About />
       <Team />
       <Portofolio />
       <Clients />
+      <GlobalClients />
     </main>
   )
 }
---
+++ src/layout/global-clients.tsx
@@ -0,0 +1,15 @@
+import SectionTitle from '@/components/misc/section-title'
+import React from 'react'
+
+const GlobalClients = () => {
+  return (
+    <section className='py-20'>
+      <SectionTitle
+        title='Trusted by Global Brands'
+        text='Lorem ipsum dolor sit amet, consectetur adipiscing elit. In convallis tortor eros. Donec vitae tortor lacus. Phasellus aliquam ante in maximus.'
+      />
+    </section>
+  )
+}
+
+export default GlobalClients
